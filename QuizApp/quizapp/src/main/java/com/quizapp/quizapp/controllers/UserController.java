d80a9528a33447df837ee0236eb7981210f5743c
unknown <karthikpn02@gmail.com>
2023-04-11 11:58:59 +0530

    added register stub

--- QuizApp/quizapp/src/main/java/com/quizapp/quizapp/controllers/UserController.java
+++ QuizApp/quizapp/src/main/java/com/quizapp/quizapp/controllers/UserController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
+import com.quizapp.quizapp.models.User;
 import com.quizapp.quizapp.services.UserService;
 
 
@@ -17,5 +20,12 @@ public class UserController {
     public String userDetails(Model model){
         model.addAttribute("users",userService.getAllUsers());
         return "users"; 
+    }
+
+    @GetMapping("/users/new")
+    public String createNewUser(Model model){
+        User user = new User();
+        model.addAttribute("user", user);
+        return "create_user";
     }
 }
